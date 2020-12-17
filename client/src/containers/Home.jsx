e85f4c4027b322e4b1a9b07d4ec8dc11ed5e174f
John Robling <roblingjohn@gmail.com>
2020-12-17 17:25:10 -0500

    created comic component

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Comic from "../components/Comic"
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is the home page</h1>
+                <Comic />
             </div>
         );
     }
