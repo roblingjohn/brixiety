5a00f2ffe740e0a2b5af582b03a5e5232ae32300
John Robling <roblingjohn@gmail.com>
2021-02-01 11:59:03 -0500

    changed get on server.js

--- server.js
+++ server.js
@@ -20,10 +20,13 @@ app.use(express.json({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*"),
-    (req, res) => {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    };
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '.client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 }
 
 // app.use(routes);
