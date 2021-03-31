9f4d60ddd6b4c667d21c521c9fe9bc92d751cb96
John Robling <roblingjohn@gmail.com>
2021-03-31 12:22:51 -0400

    added title text to comics

--- client/src/assets/comicData/comicData.js
+++ client/src/assets/comicData/comicData.js
@@ -1,13 +1,58 @@
 export default [
-  { comicNumber: 1, comicTitle: "For Fun" },
-  { comicNumber: 2, comicTitle: "Mystery Text" },
-  { comicNumber: 3, comicTitle: "Diet and Exercise" },
-  { comicNumber: 4, comicTitle: "Ready to Go" },
-  { comicNumber: 5, comicTitle: "You Can Do This" },
-  { comicNumber: 6, comicTitle: "Contactless Delivery" },
-  { comicNumber: 7, comicTitle: "Heated Seats" },
-  { comicNumber: 8, comicTitle: "Time Cook" },
-  { comicNumber: 9, comicTitle: "Important Tasks" },
-  { comicNumber: 10, comicTitle: "Relatable" },
-  { comicNumber: 11, comicTitle: "Bother" },
+  {
+    comicNumber: 1,
+    comicTitle: "For Fun",
+    titleText: `"Oh, and I make this webcomic called Brixie...hey, why are you leaving?"`,
+  },
+  {
+    comicNumber: 2,
+    comicTitle: "Mystery Text",
+    titleText: `You people and your "having multiple friends" and "actually reeaching out to them."`,
+  },
+  {
+    comicNumber: 3,
+    comicTitle: "Diet and Exercise",
+    titleText: "Maybe beer and pizza will make me feel better.",
+  },
+  {
+    comicNumber: 4,
+    comicTitle: "Ready to Go",
+    titleText: `"Sorry I'm early, Waze found me a faster route."`,
+  },
+  {
+    comicNumber: 5,
+    comicTitle: "You Can Do This",
+    titleText: `"Oh dear god, they responded!"`,
+  },
+  {
+    comicNumber: 6,
+    comicTitle: "Contactless Delivery",
+    titleText:
+      "One great thing about Pandemic World is that I can get food without ever interacting with another human being.",
+  },
+  {
+    comicNumber: 7,
+    comicTitle: "Heated Seats",
+    titleText: `On this week's episode of "Person Who Isn't Used to Having Nice Things":`,
+  },
+  {
+    comicNumber: 8,
+    comicTitle: "Time Cook",
+    titleText: "Lord help me if someone leaves time on the microwave.",
+  },
+  {
+    comicNumber: 9,
+    comicTitle: "Important Tasks",
+    titleText: `I sent this to my wife and she asked "Is that what you're doing right now?"`,
+  },
+  {
+    comicNumber: 10,
+    comicTitle: "Relatable",
+    titleText: "*looks to the window and sees Catana Chetwynd peeking in*",
+  },
+  {
+    comicNumber: 11,
+    comicTitle: "Bother",
+    titleText: `"What about Stop, Drop, and Roll?" "Eh, sounds like too much effort."`,
+  },
 ];
