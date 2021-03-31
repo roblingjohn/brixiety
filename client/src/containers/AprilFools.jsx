da5c6e3a22dbfea49e9bbfc874d752eb4abbc621
John Robling <roblingjohn@gmail.com>
2021-03-31 10:32:08 -0400

    updated april fools page

--- client/src/containers/AprilFools.jsx
+++ client/src/containers/AprilFools.jsx
@@ -5,12 +5,17 @@ const AprilFools = () => {
   return (
     <div className="container">
       <h1>APRIL FOOLS!</h1>
+      <img
+        className="aprilFoolsImg"
+        alt="April Fools!"
+        src={require("../assets/images/aprilfools.png")}
+      />
       <p className="pageText">
-        Ha! I sure fooled you! 'Twas I who made that poorly-done comic. Man, you
-        certainly fell for my ruse...
+        Ha! I sure fooled you! You were expecting a poorly made LEGO webcomic,
+        but instead you got Brixiety! Man, you certainly fell for my ruse!
       </p>
       <p className="pageText">
-        You're right. It wasn't actually that great. It was terrible like
+        Okay, you're right. It wasn't actually that great. It was terrible like
         everything else I make. I'm sorry I wasted your time. If you want to see
         actual LEGO-based comics that aren't intentionally poorly done, click{" "}
         <Link to="/comic/12">here</Link>.
