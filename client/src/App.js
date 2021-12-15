2ac34e5808f05b9f2c9ce59b3c3c818da9857048
John Robling <roblingjohn@gmail.com>
2021-12-15 13:55:32 -0500

    created blog page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Copyright from "./components/Copyright";
 import AprilFools from "./containers/AprilFools";
 import Donate from "./components/Donate";
 import GhostTemplate from "./containers/GhostTemplate";
+import Blog from "./containers/Blog"
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/comic/:id" component={ComicPage} />
           <Route exact path="/ghosttemplate" component={GhostTemplate} />
           <Route exact path="/aprilfools" component={AprilFools} />
+          <Route exact path="/blog" component={Blog} />
           <Route exact path="/" component={Home} />
           <Route path="*" component={PageNotFound} />
         </Switch>
---
+++ client/src/containers/Blog.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>This is the blog page</h1>
+            </div>
+        );
+    }
+}
+
+export default Blog;
