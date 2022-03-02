c95269566a795b5836dc34cf4b6bc21a204ac207
John Robling <roblingjohn@gmail.com>
2022-03-01 22:32:37 -0500

    Added dates to all comics.

--- client/src/assets/comicData/comicData.js
+++ client/src/assets/comicData/comicData.js
@@ -1,308 +1,365 @@
+import moment from "moment";
+
 export default [
   {
     comicNumber: 1,
+    date: "2021-01-29",
     comicTitle: "For Fun",
     titleText: `"Oh, and I make this webcomic called Brixie...hey, where are you going?"`,
   },
   {
     comicNumber: 2,
+    date: "2021-02-01",
     comicTitle: "Mystery Text",
     titleText: `You people and your "having multiple friends" and "actually reaching out to them."`,
   },
   {
     comicNumber: 3,
+    date: "2021-02-03",
     comicTitle: "Diet and Exercise",
     titleText: "Maybe beer and pizza will make me feel better.",
   },
   {
     comicNumber: 4,
+    date: "2021-02-05",
     comicTitle: "Ready to Go",
     titleText: `"Sorry I'm early, Waze found me a faster route."`,
   },
   {
     comicNumber: 5,
+    date: "2021-02-12",
     comicTitle: "You Can Do This",
     titleText: `"Oh dear god, they responded!"`,
   },
   {
     comicNumber: 6,
+    date: "2021-02-19",
     comicTitle: "Contactless Delivery",
     titleText:
       "One great thing about Pandemic World is that I can get food without ever interacting with another human being.",
   },
   {
     comicNumber: 7,
+    date: "2021-02-26",
     comicTitle: "Heated Seats",
     titleText: `On this week's episode of "Person Who Isn't Used to Having Nice Things":`,
   },
   {
     comicNumber: 8,
+    date: "2021-03-05",
     comicTitle: "Time Cook",
     titleText: "Lord help me if someone leaves time on the microwave.",
   },
   {
     comicNumber: 9,
+    date: "2021-03-12",
     comicTitle: "Important Tasks",
     titleText: `I sent this to my wife and she asked "Is that what you're doing right now?"`,
   },
   {
     comicNumber: 10,
+    date: "2021-03-19",
     comicTitle: "Relatable",
     titleText: "*looks to the window and sees Catana Chetwynd peeking in*",
   },
   {
     comicNumber: 11,
+    date: "2021-03-26",
     comicTitle: "Bother",
     titleText: `"What about Stop, Drop, and Roll?" "Eh, sounds like too much effort."`,
   },
   {
     comicNumber: 12,
+    date: "2021-04-01",
     comicTitle: "awkward lego man (April Fools)",
     titleText: "April Fools!",
   },
   {
     comicNumber: 13,
+    date: "2021-04-09",
     comicTitle: "Free Trial",
     titleText:
       "This was bad enough before COVID. I'm not looking forward to readjusting to socialization.",
   },
   {
     comicNumber: 14,
+    date: "2021-04-16",
     comicTitle: "Patient",
     titleText:
       "This does nothing to disprove my belief that I am actually just a cat.",
   },
   {
     comicNumber: 15,
+    date: "2021-04-23",
     comicTitle: "Adult",
     titleText:
       "Next you're gonna tell me it's been 10 years since I graduated college. ... OH GOD!",
   },
   {
     comicNumber: 16,
+    date: "2021-04-30",
     comicTitle: "Motivation",
     titleText:
       "I definitely didn't build a 4000 piece LEGO roller coaster instead of doing what I was supposed to this week",
   },
   {
     comicNumber: 17,
+    date: "2021-05-07",
     comicTitle: "Pacing",
     titleText:
       "You mean I get to practice imaginary conversations I'll never have *AND* I'm improving my health?",
   },
   {
     comicNumber: 18,
+    date: "2021-05-14",
     comicTitle: "Car Horn",
     titleText: "Hondas from the 2000s had such wussy horns.",
   },
   {
     comicNumber: 19,
+    date: "2021-05-21",
     comicTitle: "Easy",
     titleText:
       "We both spent the first year of our relationship just waiting for the other shoe to drop. Turns out relationships can just be easy.",
   },
   {
     comicNumber: 20,
+    date: "2021-05-28",
     comicTitle: "Old Posts",
     titleText:
       "Our parents were just as dumb as us. Their stupidity just wasn't archived.",
   },
   {
     comicNumber: 21,
+    date: "2021-06-04",
     comicTitle: "Kitty Sense",
     titleText: "Sorry I'm late I had a ca- uh, traffic.",
   },
   {
     comicNumber: 22,
+    date: "2021-06-11",
     comicTitle: "Creative Process",
     titleText: "Oh god, why did I start this stupid webcomic?",
   },
   {
     comicNumber: 23,
+    date: "2021-06-18",
     comicTitle: "Actually Cook",
     titleText: `2 weeks later: "Aw man, the ingredients I bought all went bad."`,
   },
   {
     comicNumber: 24,
+    date: "2021-06-25",
     comicTitle: "Other People",
     titleText:
       "Yep, this was definitely a thing restricted to high school. Hasn't carried over into my adult life at all.",
   },
   {
     comicNumber: 25,
+    date: "2021-07-02",
     comicTitle: "Vacation",
     titleText:
       "My actual vacation is going to be to Colorado, but my attire for that doesn't lend itself as well to the visual shorthand of going to the beach.",
   },
   {
     comicNumber: 26,
+    date: "2021-07-23",
     comicTitle: "Scam Likely",
     titleText:
       "I like to imagine Scam Likely as an actual person who cold calls people and tries to steal their money. Also he has a thin handlebar mustache.",
   },
   {
     comicNumber: 27,
+    date: "2021-07-30",
     comicTitle: "Mechanic",
     titleText: `"You're also dangerously low on blinker fluid." "Oh no, is that expensive?`,
   },
   {
     comicNumber: 28,
+    date: "2021-08-06",
     comicTitle: "Tried Harder",
     titleText: "Also known as gifted kid syndrome.",
   },
   {
     comicNumber: 29,
+    date: "2021-08-13",
     comicTitle: "Job Hunt",
     titleText:
       "Before you mention the current understaffing crisis, I'm in my 30s and have a college degree, so I ain't working retail or food service.",
   },
   {
     comicNumber: 30,
+    date: "2021-08-27",
     comicTitle: "Doing Okay",
     titleText:
       "I generally send my wife these comics for notes becuase she's good at comedy, but there have been times that they have made her generally concerned for my wellbeing.",
   },
   {
     comicNumber: 31,
+    date: "2021-09-03",
     comicTitle: "Horse Medicine",
     titleText:
       "I still haven't decided whether the centaur is offended or complimented.",
   },
   {
     comicNumber: 32,
+    date: "2021-09-10",
     comicTitle: "Right Place",
     titleText:
       "I accidentally walked into the wrong house one time several years ago, and I don't think I've ever fully recovered from that.",
   },
   {
     comicNumber: 33,
+    date: "2021-09-24",
     comicTitle: "Two Weeks",
     titleText:
       "To be fair, we weren't expecting the closing date for our house to be the same weeks as our wedding.",
   },
   {
     comicNumber: 34,
+    date: "2021-10-01",
     comicTitle: "Meditate",
     titleText:
       "Ain't it grand when you have to turn your mind of for 10 minutes just to get to a base level where you can function as a human being?",
   },
   {
     comicNumber: 35,
+    date: "2021-10-08",
     comicTitle: "October",
     titleText:
       "Can't wait for the one week of nice weather before it's suddenly freezing.",
   },
   {
     comicNumber: 36,
+    date: "2021-10-15",
     comicTitle: "Marriage",
     titleText:
       "Or maybe this is just a thing that happens when you get married in the middle of a global pandemic?",
   },
   {
     comicNumber: 37,
+    date: "2021-10-22",
     comicTitle: "Intake",
     titleText: `"And then finally we'll need you to listen to your mom on the phone with your aunt while she incorrectly explains what's going on in your life"`,
   },
   {
     comicNumber: 38,
+    date: "2021-10-29",
     comicTitle: "Haunted House",
     titleText:
       "There is truly nothing scarier than the constant linear flow of time.",
   },
   {
     comicNumber: 39,
+    date: "2021-11-05",
     comicTitle: "HEY!",
     titleText: "Sorry, I didn't have a Rescue Helicopter to build.",
   },
   {
     comicNumber: 40,
+    date: "2021-11-12",
     comicTitle: "Change",
     titleText:
       "God grant be the anxiety to complain about the things I cannot change, the laziness to complain about the the things I can, and the depression to wonder why nothing gets better.",
   },
   {
     comicNumber: 41,
+    date: "2021-11-19",
     comicTitle: "Chocolate",
     titleText: "Plot twist: the dog actually poops out soft serve.",
   },
   {
     comicNumber: 42,
+    date: "2021-11-26",
     comicTitle: "Jolly",
     titleText:
       "Am I a joyless Scrooge for ruining the enjoyment of people who want to celebrate early? Yes. Am I also a major proponent of strict delineation between holiday seasons? Also yes.",
   },
   {
     comicNumber: 43,
+    date: "2021-12-03",
     comicTitle: "Imposter Syndrome",
     titleText:
       "And that dumb webcomic you make? No one really likes it. It's just bots upvoting it.",
   },
   {
     comicNumber: 44,
+    date: "2021-12-10",
     comicTitle: "Content Creation",
     titleText:
       "It seems like it's the ones I'm not super enthusiastic about that always end up being popular.",
   },
   {
     comicNumber: 45,
+    date: "2021-12-17",
     comicTitle: "Supply Chain",
     titleText:
       "Sorry reader, I couldn't think of a title text. Supply chain issues, y'know?",
   },
   {
     comicNumber: 46,
+    date: "2021-12-24",
     comicTitle: "Christmas List",
     titleText:
       "This is even worse than last year, when my present was my heart, and less than 24 hours later you had already regifted it.",
   },
   {
     comicNumber: 47,
+    date: "2021-12-31",
     comicTitle: "Doing Things",
     titleText:
       "But then how will I work myself up to be ready for it then be completely thrown off when it doesn't go as expected?",
   },
   {
     comicNumber: 48,
+    date: "2022-01-07",
     comicTitle: "Cynical",
     titleText: "Later I'll look up tips on how to stop procrastinating.",
   },
   {
     comicNumber: 49,
+    date: "2022-01-14",
     comicTitle: "Podcast",
     titleText: `"That's ridiculous. Do you really thing I would tell you if I was?"`,
   },
   {
     comicNumber: 50,
+    date: "2022-01-28",
     comicTitle: "Charge and Drain",
     titleText:
       "Being introverted doesn't mean you hate being around people, it just means you only want to be around people for a certain amount of time.",
   },
   {
     comicNumber: 51,
+    date: "2022-02-04",
     comicTitle: "Boba",
     titleText:
       "Yes, it's usually referred to as either boba or bubble tea, not boba tea. I'm a pedant myself. But Wikipedia has it as an acceptable term, so I'm going with it.",
   },
   {
     comicNumber: 52,
+    date: "2022-02-11",
     comicTitle: "Sportsball",
     titleText:
       "Friendly reminder that hating things is not a personality trait.",
   },
   {
     comicNumber: 53,
+    date: "2022-02-18",
     comicTitle: "Executive Dysfunction",
     titleText:
       "My psychologist says I can get an Adderal prescription once I see a physician, but for some reason I haven't gotten around to doing that.",
   },
   {
     comicNumber: 54,
+    date: "2022-02-25",
     comicTitle: "Gassy",
     titleText:
       "I mean, sure there are other advantages to eating healthy, but I really thought that it would at least make me fart less.",
   },
   {
     comicNumber: 55,
+    date: "2022-03-04",
     comicTitle: "Plants",
     titleText:
       "The sequel to this comic will feature her being suprised when our cat tries to eat them all.",
