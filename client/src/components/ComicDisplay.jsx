39de29515ab324f061ecf54df851f13876744e35
John Robling <roblingjohn@gmail.com>
2023-06-12 20:55:08 -0400

    Refactors comic display pages.

--- client/src/components/ComicDisplay.jsx
+++ client/src/components/ComicDisplay.jsx
@@ -1,50 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Comic from "./Comic";
-import NavButtons from "./NavButtons";
+import NavButtons from "../components/NavButtons";
 
-class ComicDisplay extends Component {
-  state = {
-    comic: {
-      id: this.props.id,
-    },
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    if (props.id !== state.comic.id) {
-      return {
-        comic: {
-          id: props.id,
-        },
-      };
-    }
-    return null;
-  }
-
-  componentDidMount() {
-    this.setState({
-      comic: {
-        id: this.props.id,
-      },
-    });
-  }
-
-  comicId = parseInt(this.state.comic.id);
-
-  render() {
-    return (
-      <div
-        className={
-          this.state.comic.id === 12 || this.state.comic.id === "12" || this.state.comic.id === 58 || this.state.comic.id === "58"
-            ? "aprilFools"
-            : ""
-        }
-      >
-        <NavButtons id={this.state.comic.id} />
-        <Comic id={this.state.comic.id} />
-        <NavButtons id={this.state.comic.id} />
-      </div>
-    );
-  }
-}
+const ComicDisplay = (props) => {
+  return (
+    <div>
+        <NavButtons />
+        <Comic />
+        <NavButtons />
+    </div>
+  );
+};
 
 export default ComicDisplay;
--- client/src/containers/ComicPage.jsx
+++ client/src/containers/ComicPage.jsx
@@ -1,60 +1,19 @@
-import React, { Component } from "react";
-import ComicDisplay from "../components/ComicDisplay";
+import React, { createContext } from "react";
 import comicData from "../assets/comicData/comicData";
+import ComicDisplay from "../components/ComicDisplay";
 
-class ComicPage extends Component {
-  state = {
-    comic: {
-      id: parseInt(this.props.match.params.id),
-    },
-  };
+export const ComicContext = createContext();
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.match.params.id !== state.comic.id) {
-      return {
-        comic: {
-          id: props.match.params.id,
-        },
-      };
-    }
-    return null;
-  }
-
-  componentDidUpdate = (nextProps) => {
-    if (nextProps.match.params.id !== this.props.match.params.id) {
-      this.setState({
-        comic: {
-          id: parseInt(this.props.match.params.id),
-        },
-      });
-    }
-    document.title = `Brixiety | #${this.state.comic.id}: ${
-      comicData[this.state.comic.id - 1].comicTitle
-    }`;
-  };
-
-  componentDidMount() {
-    if (this.props.match.params.id !== this.state.comic.id) {
-      this.setState({
-        comic: {
-          id: parseInt(this.props.match.params.id),
-        },
-      });
-    }
-    document.title = `Brixiety | #${this.state.comic.id}: ${
-      comicData[this.state.comic.id - 1].comicTitle
-    }`;
-  }
-
-  comicId = this.state.comic.id;
-
-  render() {
-    return (
-      <div className={this.state.comic.id === 12 ? "aprilFools" : ""}>
-        <ComicDisplay id={this.state.comic.id} />
-      </div>
-    );
-  }
-}
+const ComicPage = (props) => {
+  return (
+    <div>
+      <ComicContext.Provider
+        value={comicData[parseInt(props.match.params.id) - 1]}
+      >
+        <ComicDisplay id={props.match.params.id} />
+      </ComicContext.Provider>
+    </div>
+  );
+};
 
 export default ComicPage;
