d8a2144b13fcb2a4a37623c79e065ce87c77ace4
John Robling <roblingjohn@gmail.com>
2023-09-07 20:28:51 -0400

    Adds number and date to comic display.

--- client/src/components/Comic.jsx
+++ client/src/components/Comic.jsx
@@ -1,12 +1,18 @@
+import moment from "moment";
 import React, { useContext, useState } from "react";
 import { ComicContext } from "../components/ComicDisplay";
 
 const Comic = () => {
-  const { comicTitle, titleText, comicNumber } = useContext(ComicContext);
+  const { comicTitle, titleText, comicNumber, date } = useContext(ComicContext);
   const [isExpanded, toggleExpand] = useState(false);
   return (
     <div>
-      <h1>{comicTitle}</h1>
+      <h1 class="mb-0">
+        <b>{comicTitle}</b>
+      </h1>
+      <div class="mb-1">
+        #{comicNumber} | {moment(date).format("MMMM D, YYYY")}
+      </div>
       <img
         src={require(`../assets/comics/${comicNumber}.png`)}
         className={isExpanded ? "comicImgExpanded" : "comicImg"}
