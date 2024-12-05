5c8f793dbe85dba294f0010a10f1b36667f3e7bf
John Robling <roblingjohn@gmail.com>
2024-12-05 12:12:46 -0500

    Revamps Socials page.

--- client/src/containers/Socials.jsx
+++ client/src/containers/Socials.jsx
@@ -1,134 +1,104 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
-class Socials extends Component {
-  componentDidMount() {
+const Socials = () => {
+  useEffect(() => {
     document.title = "Brixiety | Socials";
-  }
-  render() {
-    return (
-      <div className="container">
-        <h1>Social Media</h1>
-        <p>
-          These are all the official social media pages for Brixiety. Anyone
-          else claiming to be us is fake.
-        </p>
-        <ul className="pageText">
-          <li>
-            <strong>
-              Donate at{" "}
-              <a
-                href="https://www.buymeacoffee.com/brixiety"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Buy Me a Coffee
-              </a>
-            </strong>
-          </li>
-          <li>
-            email:{" "}
-            <a href="mailto:brixiety.comic@gmail.com">
-              brixiety.comic@gmail.com
+  });
+  const items = [
+    {
+      website: "Donate at",
+      url: "https://www.buymeacoffee.com/brixiety",
+      name: "Buy Me a Coffee",
+      specialClass: "font-weight-bold",
+    },
+    {
+      website: "email",
+      url: "mailto:brixiety.comic@gmail.com",
+      name: "brixiety.comic@gmail.com",
+    },
+    {
+      website: "Facebook",
+      url: "https://www.facebook.com/brixiety",
+      name: "brixiety",
+    },
+    {
+      website: "Instagram",
+      url: "https://www.instagram.com/brixiety.comic/",
+      name: "@brixiety.comic",
+      secondaryUrl: "https://www.instagram.com/john.brixiety/",
+      secondaryName: "@john.brixiety",
+      secondaryNote: "non-comic LEGO stuff",
+    },
+    {
+      website: "Bluesky",
+      url: "https://bsky.app/profile/brixiety.com",
+      name: "@brixiety.com",
+    },
+    {
+      website: "Reddit",
+      url: "https://reddit.com/r/brixiety/",
+      name: "/r/brixiety",
+    },
+    {
+      website: "Imgur",
+      url: "https://imgur.com/user/brixiety",
+      name: "brixiety",
+    },
+    {
+      website: "YouTube",
+      url: "https://www.youtube.com/channel/UCSdVQTD0VmEh0bsws6H5Rag/",
+      name: "Brixiety Studios",
+      note: "coming eventually?",
+    },
+    {
+      website: "Tumblr",
+      url: "https://brixiety.tumblr.com/",
+      name: "brixiety",
+      note: "defunct",
+      specialClass: "font-italic",
+    },
+    {
+      website: "Twitter",
+      url: "https://twitter.com/BrixietyComic",
+      name: "@BrixietyComic",
+      note: "defunct",
+      specialClass: "font-italic",
+    },
+  ];
+  return (
+    <div className="container">
+      <h1>Social Media</h1>
+      <p>
+        These are all the official social media pages for Brixiety. Anyone else
+        claiming to be us is fake.
+      </p>
+      <ul className="pageText">
+        {items.map((item) => (
+          <li className={item?.specialClass || ""}>
+            {item.website}:{" "}
+            <a href={item.url} target="_blank" rel="noreferrer noopener">
+              {item.name}
             </a>
-          </li>
-          <li>
-            Facebook:{" "}
-            <a
-              href="https://www.facebook.com/brixiety"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              brixiety
-            </a>
-          </li>
-          <li>
-            Instagram:{" "}
-            <a
-              href="https://www.instagram.com/brixiety.comic/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              @brixiety.comic
-            </a>{" "}
-            {" | "}{" "}
-            <a
-              href="https://www.instagram.com/john.brixiety/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              @john.brixiety
-            </a>{" "}
-            {" (non-comic LEGO stuff)"}
-          </li>
-          <li>
-            Bluesky:{" "}
-            <a
-              href="https://bsky.app/profile/brixiety.com"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              @brixiety.com
-            </a>
-          </li>
+            {item?.note && ` (${item.note})`}
+            {item.secondaryUrl && (
+              <span>
+                {" | "}
 
-          <li>
-            Reddit:{" "}
-            <a
-              href="https://reddit.com/r/brixiety/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              /r/brixiety
-            </a>
+                <a
+                  href={item.secondaryUrl}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  {item.secondaryName}
+                </a>
+              </span>
+            )}
+            {item?.secondaryNote && ` (${item.secondaryNote})`}
           </li>
-          <li>
-            Imgur:{" "}
-            <a
-              href="https://imgur.com/user/brixiety"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              brixiety
-            </a>
-          </li>
-          <li>
-            YouTube:{" "}
-            <a
-              href="https://www.youtube.com/channel/UCSdVQTD0VmEh0bsws6H5Rag/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Brixiety Studios
-            </a>{" "}
-            (coming soon!)
-          </li>
-          
-          <li>
-            Tumblr:{" "}
-            <a
-              href="https://brixiety.tumblr.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              brixiety
-            </a>{" "}
-            (defunct)
-          </li>
-          <li>
-            Twitter:{" "}
-            <a
-              href="https://twitter.com/BrixietyComic"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              @BrixietyComic
-            </a>{" "}
-            (defunct)
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Socials;
